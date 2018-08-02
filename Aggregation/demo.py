92ee7d479501a24214b3bee8e413107a65142325
Luke Olson <luke.olson@gmail.com>
2018-08-01 20:48:38 -0500

    simplify plotting with linecollection

--- Aggregation/demo.py
+++ Aggregation/demo.py
@@ -1,34 +1,47 @@
-# Illustrates the selection of aggregates in AMG based on smoothed aggregation
+"""
+Illustrates the selection of aggregates in AMG based on smoothed aggregation
+"""
 
 import numpy
-from scipy.io import loadmat
-from pyamg import smoothed_aggregation_solver
-from pyamg.gallery import load_example
+import scipy.io as sio
+import matplotlib as mplt
+import matplotlib.pyplot as plt
+import pyamg
 
-data = loadmat('square.mat')
+data = sio.loadmat('square.mat')
 
-A = data['A'].tocsr()                        # matrix
-V = data['vertices'][:A.shape[0]]            # vertices of each variable
+A = data['A'].tocsr()                              # matrix
+V = data['vertices'][:A.shape[0]]                  # vertices of each variable
 E = numpy.vstack((A.tocoo().row,A.tocoo().col)).T  # edges of the matrix graph
 
-# Use Smoothed Aggregation Algorithm
-mls = smoothed_aggregation_solver(A, max_levels=2, max_coarse=1, keep=True)
+# Create a multigrid solver
+ml = pyamg.smoothed_aggregation_solver(A, max_levels=2, max_coarse=1, keep=True)
 
 # AggOp[i,j] is 1 iff node i belongs to aggregate j
-AggOp = mls.levels[0].AggOp
+AggOp = ml.levels[0].AggOp
 
 # determine which edges lie entirely inside an aggregate
-inner_edges = AggOp.indices[E[:,0]] == AggOp.indices[E[:,1]]  #AggOp.indices[n] is the aggregate to which vertex n belongs
-outer_edges = -inner_edges
+# AggOp.indices[n] is the aggregate to which vertex n belongs
+inner_edges = AggOp.indices[E[:,0]] == AggOp.indices[E[:,1]]
+outer_edges = ~inner_edges
 
-from draw import lineplot
-from pylab import figure, axis, scatter, show
+# set up a figure
+fig, ax = plt.subplots(figsize=(6, 6))
 
-figure(figsize=(6,6))
-axis('equal')
-lineplot(V, E[inner_edges], linewidths=3.0)
-lineplot(V, E[outer_edges], linewidths=0.2)
-scatter(V[:,0], V[:,1], c='r', s=100.0)
-#scatter(V[:,0], V[:,1], c=numpy.ones(V.shape[0]), s=100.0)
-show()
- 
+# non aggregate edges
+nonaggs = V[E[outer_edges].ravel(),:].reshape((-1, 2, 2))
+col = mplt.collections.LineCollection(nonaggs,
+                                      color=[232.0/255, 74.0/255, 39.0/255],
+                                      linewidth=1.0)
+ax.add_collection(col, autolim=True)
+
+# aggregate edges
+aggs = V[E[inner_edges].ravel(),:].reshape((-1, 2, 2))
+col = mplt.collections.LineCollection(aggs,
+                                      color=[19.0/255, 41.0/255, 75.0/255],
+                                      linewidth=4.0)
+ax.add_collection(col, autolim=True)
+
+ax.autoscale_view()
+ax.axis('equal')
+plt.show()
