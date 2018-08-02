ca69e1f024cad8896c717b847ea1473b783c4927
Luke Olson <luke.olson@gmail.com>
2018-08-02 13:15:26 -0500

    clean up helmholtz example

--- ComplexSymmetric/my_vis.py
+++ ComplexSymmetric/my_vis.py
@@ -13,19 +13,18 @@ __docformat__ = "restructuredtext en"
 
 import warnings
 
-from numpy import array, ones, zeros, sqrt, asarray, empty, concatenate, \
-        random, uint8, kron, arange, diff, c_, where, issubdtype, \
-        integer, mean, sum, prod, ravel, hstack, invert, repeat, floor
+import numpy as np
 
 from scipy.spatial import Delaunay
 from scipy import array, zeros, mean, kron, ones, sparse, rand
 from scipy.sparse import csr_matrix, coo_matrix, csc_matrix
 from os import system
-# pyamg
-from pyamg.vis import write_basic_mesh, write_vtu
-from pyamg.util.utils import scale_rows, scale_columns
+
+import pyamg
+import pyamg.vis
 
 __all__ = ['my_vis', 'shrink_elmts', 'dg_vis']
+
 
 def shrink_elmts(E2V, Vert, shrink=0.75):
     """
@@ -53,15 +52,15 @@ def shrink_elmts(E2V, Vert, shrink=0.75):
 
     if(Vert.shape[1] == 2):
         Dimen = 2
-        #Determine if polynomial order is greater than 1
+        # Determine if polynomial order is greater than 1
         if(Nelnodes > 3):
             nonlin = True
             num_non_verts = Nelnodes - 3
         else:
             nonlin = False
-    elif(Vert[:,2].nonzero()[0].shape[0] == 0):   #Assume 2D if last column of Vert is all zero
+    elif(Vert[:, 2].nonzero()[0].shape[0] == 0):  # Assume 2D if last column of Vert is all zero
         Dimen = 2
-        #Determine if polynomial order is greater than 1
+        # Determine if polynomial order is greater than 1
         if(Nelnodes > 3):
             nonlin = True
             num_non_verts = Nelnodes - 3
@@ -69,7 +68,7 @@ def shrink_elmts(E2V, Vert, shrink=0.75):
             nonlin = False
     else:
         Dimen = 3
-        #Determine if polynomial order of basis functions is greater than 1
+        # Determine if polynomial order of basis functions is greater than 1
         if(Nelnodes > 4):
             nonlin = True
             num_non_verts = Nelnodes - 4
@@ -77,32 +76,32 @@ def shrink_elmts(E2V, Vert, shrink=0.75):
             nonlin = False
 
     # Account for shared faces, for case that this is used to shrink a cont Gal mesh
-    #Vert = Vert[E2V.flatten(),:]
-    #Agg = Agg[E2V.flatten(),:]
-    #E2V = array(range(Vert.shape[0])).reshape(Vert.shape[0]/Nelnodes, Nelnodes)
-    #Nel = E2V.shape[0]
-    
-    #Store Barycenter for each element
+    # Vert = Vert[E2V.flatten(),:]
+    # Agg = Agg[E2V.flatten(),:]
+    # E2V = array(range(Vert.shape[0])).reshape(Vert.shape[0]/Nelnodes, Nelnodes)
+    # Nel = E2V.shape[0]
+
+    # Store Barycenter for each element
     Bcenter = zeros((Nel, Vert.shape[1]))
 
     for i in range(Nel):
-        #Assumes first Dimen+1 nodes are verts for the simplex
-        verts_K = Vert[E2V[i,0:(Dimen+1)], :]
-        
-        #Calculate Barycenter of element i
-        Bcenter[i,:] = mean(verts_K, 0)
-        
-        #Shift vertices to barycenter
-        Vert[E2V[i,0:Dimen+1],:] = shrink*verts_K + (1-shrink)*kron(Bcenter[i,:], ones((Dimen+1,1)) )
+        # Assumes first Dimen+1 nodes are verts for the simplex
+        verts_K = Vert[E2V[i, 0:(Dimen + 1)], :]
+
+        # Calculate Barycenter of element i
+        Bcenter[i, :] = mean(verts_K, 0)
+
+        # Shift vertices to barycenter
+        Vert[E2V[i, 0:Dimen + 1], :] = shrink * verts_K + \
+            (1 - shrink) * kron(Bcenter[i, :], ones((Dimen + 1, 1)))
 
         if(nonlin):
             # Move non-vertices to barycenter with the same formula, namely
             #    shrink*point_barycoords + (1-shrink)*barycenter.
-            Vert[ E2V[i, (Dimen+1):], :] = shrink*(Vert[ E2V[i, (Dimen+1):], :]) + \
-                                       (1-shrink)*kron(Bcenter[i,:], ones((num_non_verts,1)) )
-    
-    return E2V, Vert
+            Vert[E2V[i, (Dimen + 1):], :] = shrink * (Vert[E2V[i, (Dimen + 1):], :]) + \
+                (1 - shrink) * kron(Bcenter[i, :], ones((num_non_verts, 1)))
 
+    return E2V, Vert
 
 
 def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
@@ -117,7 +116,7 @@ def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
     E2V : {array}
         element index array (Nel x Nelnodes)
     Agg : {csr_matrix}
-        sparse matrix for the aggregate-vertex relationship (N x Nagg)  
+        sparse matrix for the aggregate-vertex relationship (N x Nagg)
     mesh_type : {string}
         type of elements: tri
     A : {sparse amtrix}
@@ -126,7 +125,7 @@ def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
     Returns
     -------
         - Writes data to two .vtk files for use in Paraview (xml 0.1 format)
-    
+
     Notes
     -----
 
@@ -134,14 +133,12 @@ def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
     Examples
     --------
 
-     """
-
-    #----------------------
-    if not issubdtype(Vert.dtype,float):
+    """
+    if not np.issubdtype(Vert.dtype, np.floating):
         raise ValueError('Vert should be of type float')
 
     if E2V is not None:
-        if not issubdtype(E2V.dtype,integer):
+        if not np.issubdtype(E2V.dtype, np.signedinteger):
             raise ValueError('E2V should be of type integer')
 
     if Agg.shape[1] > Agg.shape[0]:
@@ -149,83 +146,86 @@ def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
 
     valid_mesh_types = ('tri')
     if mesh_type not in valid_mesh_types:
-        raise ValueError('mesh_type should be %s' % ' or '.join(valid_mesh_types))
+        raise ValueError(
+            'mesh_type should be %s' %
+            ' or '.join(valid_mesh_types))
 
     if A is not None:
         if (A.shape[0] != A.shape[1]) or (A.shape[0] != Agg.shape[0]):
-            raise ValueError('expected square matrix A and compatible with Agg')
+            raise ValueError(
+                'expected square matrix A and compatible with Agg')
 
-    #----------------------
-
-    N        = Vert.shape[0]
-    Ndof     = N
+    N = Vert.shape[0]
+    Ndof = N
     if E2V is not None:
-        Nel      = E2V.shape[0]
-        Nelnodes = E2V.shape[1]
         if E2V.min() != 0:
-            warnings.warn('element indices begin at %d' % E2V.min() )
-    Nagg = Agg.shape[0]
+            warnings.warn('element indices begin at %d' % E2V.min())
 
-    Ncolors  = 16 # number of colors to use in the coloring algorithm
+    Ncolors = 16  # number of colors to use in the coloring algorithm
 
+    # Shrink each element in the mesh for nice plotting
+    # E2V, Vert = shrink_elmts(E2V, Vert)
 
-    # ------------------
-    
-    #Shrink each element in the mesh for nice plotting
-    #E2V, Vert = shrink_elmts(E2V, Vert)
-
-    # plot_type = 'vertex' output to .vtu --- throw point list down on mesh, so E2V becomes Nx1 array
+    # plot_type = 'vertex' output to .vtu --- throw point list down on mesh,
+    # so E2V becomes Nx1 array
     filename = fname + "_point-aggs.vtu"
-    if False:#A is not None:
+    if False:  # A is not None:
         # color aggregates with vertex coloring
         G = Agg.T * abs(A) * Agg
-        colors = vertex_coloring(G, method='LDF')
+        colors = pyamg.graph.vertex_coloring(G, method='LDF')
         pdata = Agg * colors  # extend aggregate colors to vertices
     else:
         # color aggregates in sequence
-        Agg   = coo_matrix(Agg)
+        Agg = coo_matrix(Agg)
         pdata = zeros(Ndof)
         colors = array(range(Agg.shape[1])) % Ncolors
         pdata[Agg.row] = Agg.col % Ncolors
 
-    write_basic_mesh(Vert, E2V=array(range(N)).reshape(N,1), mesh_type='vertex', pdata=pdata, fname=filename)
+    pyamg.vis.write_basic_mesh(
+        Vert,
+        E2V=array(range(N)).reshape(N, 1),
+        mesh_type='vertex',
+        pdata=pdata,
+        fname=filename)
 
     # plot_type = 'primal', using a global Delaunay triangulation of the shrunken mesh,
     #   we visualize the aggregates as if the global Delaunay triangulation defined a continuous Galerkin
     #   mesh upon which our aggregates are defined.
-    #tri_pts = Delaunay(Vert[:,0:2]).simplices
-    #coarse_grid_vis(filename, Vert, tri_pts, Agg, A=A, plot_type='primal', mesh_type='tri')
+    # tri_pts = Delaunay(Vert[:,0:2]).simplices
+    # coarse_grid_vis(filename, Vert, tri_pts, Agg, A=A, plot_type='primal', mesh_type='tri')
     filename = fname + "_aggs.vtu"
 
-    # Do a local Delaunay triangulation for each aggregate, and throw that down as an element in a new mesh
+    # Do a local Delaunay triangulation for each aggregate, and throw that
+    # down as an element in a new mesh
     Agg = csc_matrix(Agg)
-    E2Vnew = zeros((0,),dtype=int)
-    colors_new = zeros((0,),dtype=int)
+    E2Vnew = zeros((0,), dtype=int)
+    colors_new = zeros((0,), dtype=int)
     for i in range(Agg.shape[1]):
         rowstart = Agg.indptr[i]
-        rowend = Agg.indptr[i+1]
-        #The nonzeros in column i of Agg define the dofs in Agg i
+        rowend = Agg.indptr[i + 1]
+        # The nonzeros in column i of Agg define the dofs in Agg i
         members = Agg.indices[rowstart:rowend]
         if max(members.shape) > 2:
-            tri_pts = Delaunay(Vert[members,0:2]).simplices
-            #if i == 0:
+            tri_pts = Delaunay(Vert[members, 0:2]).simplices
+            # if i == 0:
             #    E2Vnew = ravel(members[ravel(tri_pts)])
             #    colors_new = ravel(repeat(colors[i], tri_pts.shape[0]))
-            #else:    
-            E2Vnew = hstack( (E2Vnew, ravel(members[ravel(tri_pts)]) ) )
-            colors_new = hstack( (colors_new, ravel(repeat(colors[i], tri_pts.shape[0]))) ) 
+            # else:
+            E2Vnew = np.hstack((E2Vnew, np.ravel(members[np.ravel(tri_pts)])))
+            colors_new = np.hstack(
+                (colors_new, np.ravel(np.repeat(colors[i], tri_pts.shape[0]))))
 
         if max(members.shape) == 2:
-            #create a dummy element, so that only a line is drawn between these two points in paraview
-            #if i == 0:
+            # create a dummy element, so that only a line is drawn between these two points in paraview
+            # if i == 0:
             #    E2Vnew = array([0,members[0],members[1]])
             #    colors_new = array([colors[i]])
-            #else:
-            E2Vnew = hstack( (E2Vnew, array([0,members[0],members[1]]) ) )
-            colors_new = hstack( (colors_new, colors[i]) )
+            # else:
+            E2Vnew = np.hstack((E2Vnew, array([0, members[0], members[1]])))
+            colors_new = np.hstack((colors_new, colors[i]))
 
-    #Begin Primal plotting
-    E2V = E2Vnew.reshape(-1,3)
+    # Begin Primal plotting
+    E2V = E2Vnew.reshape(-1, 3)
     Agg = csr_matrix(Agg)
 
     if E2V.max() >= Agg.shape[0]:
@@ -235,35 +235,42 @@ def dg_vis(fname, Vert, E2V, Agg, mesh_type, A=None):
     # Find elements with all vertices in same aggregate
     if len(Agg.indices) != Agg.shape[0]:
         # account for 0 rows.  Mark them as solitary aggregates
-        full_aggs = array(Agg.sum(axis=1),dtype=int).ravel()
-        full_aggs[full_aggs==1] = Agg.indices
-        full_aggs[full_aggs==0] = Agg.shape[1] + arange(0,Agg.shape[0]-Agg.nnz,dtype=int).ravel()
+        full_aggs = array(Agg.sum(axis=1), dtype=int).ravel()
+        full_aggs[full_aggs == 1] = Agg.indices
+        full_aggs[full_aggs == 0] = Agg.shape[1] + \
+            np.arange(0, Agg.shape[0] - Agg.nnz, dtype=int).ravel()
         ElementAggs = full_aggs[E2V]
     else:
         ElementAggs = Agg.indices[E2V]
-    
+
     # mask[i] == True if all vertices in element i belong to the same aggregate
-    mask = (ElementAggs[:,:-1] == ElementAggs[:,1:]).all(axis=1)
-    E2V3 = E2V[mask,:]
-    Nel3 = E2V3.shape[0]
+    mask = (ElementAggs[:, :-1] == ElementAggs[:, 1:]).all(axis=1)
+    E2V3 = E2V[mask, :]
 
     # 3 edges = 4 nodes.  Find where the difference is 0 (bdy edge)
-    markedges = diff(c_[ElementAggs,ElementAggs[:,0]])
-    markedges[mask,:]=1
-    markedelements, markededges = where(markedges==0)
+    markedges = np.diff(np.c_[ElementAggs, ElementAggs[:, 0]])
+    markedges[mask, :] = 1
+    markedelements, markededges = np.where(markedges == 0)
 
     # now concatenate the edges (i.e. first and next one (mod 3 index)
-    E2V2 = c_[[E2V[markedelements,markededges], 
-               E2V[markedelements,(markededges+1)%3]]].T 
-    Nel2 = E2V2.shape[0]
+    E2V2 = np.c_[[E2V[markedelements, markededges],
+                 E2V[markedelements, (markededges + 1) % 3]]].T
 
-    colors2 = colors_new[markedelements]  #2*ones((1,Nel2))  # color edges with twos
-    colors3 = colors_new[mask]  #3*ones((1,Nel3))  # color triangles with threes
+    # 2*ones((1,Nel2))  # color edges with twos
+    colors2 = colors_new[markedelements]
+    # 3*ones((1,Nel3))  # color triangles with threes
+    colors3 = colors_new[mask]
 
-    Cells  =  {3: E2V2, 5: E2V3}
-    cdata  =  {3: colors2, 5: colors3}
+    Cells = {3: E2V2, 5: E2V3}
+    cdata = {3: colors2, 5: colors3}
 
-    write_vtu(Verts=Vert, Cells=Cells, pdata=None, cdata=cdata, pvdata=None, fname=filename)
+    pyamg.vis.write_vtu(
+        Verts=Vert,
+        Cells=Cells,
+        pdata=None,
+        cdata=cdata,
+        pvdata=None,
+        fname=filename)
 
 
 def my_vis(ml, V, error=None, fname="", E2V=None, Pcols=None):
@@ -294,7 +301,7 @@ def my_vis(ml, V, error=None, fname="", E2V=None, Pcols=None):
     Returns
     -------
         - Writes data to .vtk files for use in Paraview (xml 0.1 format)
-    
+
     Notes
     -----
 
@@ -307,14 +314,14 @@ def my_vis(ml, V, error=None, fname="", E2V=None, Pcols=None):
 
     ##
     # For the purposes of clearer plotting, perturb vertices slightly
-    V += rand(V.shape[0], V.shape[1])*1e-6
+    V += rand(V.shape[0], V.shape[1]) * 1e-6
 
-    ## 
+    ##
     # Create a list of vertices and meshes for all levels
     levels = ml.levels
     Vlist = [V]
     if E2V is None:
-        E2V = Delaunay(V[:,0:2]).simplices
+        E2V = Delaunay(V[:, 0:2]).simplices
     E2Vlist = [E2V]
 
     mesh_type_list = []
@@ -326,41 +333,43 @@ def my_vis(ml, V, error=None, fname="", E2V=None, Pcols=None):
         mesh_type_list.append('tri')
         mesh_num_list.append(5)
     if E2V.shape[1] == 4:
-        if vertices.shape[1] == 2:
+        if V.shape[1] == 2:
             mesh_type_list.append('quad')
             mesh_num_list.append(9)
-    
+
     if sparse.isspmatrix_bsr(levels[0].A):
         nPDEs = levels[0].A.blocksize[0]
     else:
         nPDEs = 1
-    
+
     Agglist = []
-    Agg = sparse.eye(levels[0].A.shape[0]/nPDEs, levels[0].A.shape[1]/nPDEs, format='csr') 
-    for i in range(1,len(levels)):
+    Agg = sparse.eye(
+        levels[0].A.shape[0] / nPDEs,
+        levels[0].A.shape[1] / nPDEs,
+        format='csr')
+    for i in range(1, len(levels)):
         ##
         # Interpolate the vertices to the next level by taking each
         # aggregate's center of gravity (i.e. average x and y value).
-        Agg = Agg.tocsr()*levels[i-1].AggOp.tocsr()
+        Agg = Agg.tocsr() * levels[i - 1].AggOp.tocsr()
         Agg.data[:] = 1.0
         Agglist.append(Agg)
-            
-        AggX = scale_rows(Agg, Vlist[0][:,0], copy=True) 
-        AggY = scale_rows(Agg, Vlist[0][:,1], copy=True) 
-        AggX = ones((1, AggX.shape[0]))*AggX
-        AggY = ones((1, AggY.shape[0]))*AggY
-        Agg = Agg.tocsc()
-        count = Agg.indptr[1:]-Agg.indptr[:-1]
-        AggX = (ravel(AggX)/count).reshape(-1,1)
-        AggY = (ravel(AggY)/count).reshape(-1,1)
-        Vlist.append(hstack((AggX, AggY)))
 
-        E2Vnew = Delaunay(Vlist[i][:,0:2]).simplices
+        AggX = pyamg.util.scale_rows(Agg, Vlist[0][:, 0], copy=True)
+        AggY = pyamg.util.scale_rows(Agg, Vlist[0][:, 1], copy=True)
+        AggX = ones((1, AggX.shape[0])) * AggX
+        AggY = ones((1, AggY.shape[0])) * AggY
+        Agg = Agg.tocsc()
+        count = Agg.indptr[1:] - Agg.indptr[:-1]
+        AggX = (np.ravel(AggX) / count).reshape(-1, 1)
+        AggY = (np.ravel(AggY) / count).reshape(-1, 1)
+        Vlist.append(np.hstack((AggX, AggY)))
+
+        E2Vnew = Delaunay(Vlist[i][:, 0:2]).simplices
         E2Vlist.append(E2Vnew)
         mesh_type_list.append('tri')
         mesh_num_list.append(5)
 
-        
     ##
     # On each level, output aggregates, B, the mesh
     for i in range(len(levels)):
@@ -369,49 +378,47 @@ def my_vis(ml, V, error=None, fname="", E2V=None, Pcols=None):
         vertices = Vlist[i]
         elements = E2Vlist[i]
         # Print mesh
-        write_basic_mesh(vertices, elements, mesh_type=mesh_type, \
-                             fname=fname+"mesh_lvl"+str(i)+".vtu")    
+        pyamg.vis.write_basic_mesh(vertices, elements, mesh_type=mesh_type,
+                                   fname=fname + "mesh_lvl" + str(i) + ".vtu")
         # Visualize the aggregates
-        if i != (len(levels)-1):
-            dg_vis(fname+"aggs_lvl"+str(i), Vlist[0], \
-                    E2Vlist[0], Agglist[i], mesh_type)
+        if i != (len(levels) - 1):
+            dg_vis(fname + "aggs_lvl" + str(i), Vlist[0],
+                   E2Vlist[0], Agglist[i], mesh_type)
         # Visualize B
         if sparse.isspmatrix_bsr(levels[i].A):
             nPDEs = levels[i].A.blocksize[0]
         else:
             nPDEs = 1
-        cell_stuff = {mesh_num : elements}
+        cell_stuff = {mesh_num: elements}
         for j in range(nPDEs):
-            indys = arange(j,levels[i].A.shape[0],nPDEs)
-            write_vtu(Verts=vertices, Cells=cell_stuff, pdata=levels[i].B[indys,:], \
-                          fname=fname+"B_variable"+str(j)+"_lvl"+str(i)+".vtu")
+            indys = np.arange(j, levels[i].A.shape[0], nPDEs)
+            pyamg.vis.write_vtu(Verts=vertices,
+                                Cells=cell_stuff,
+                                pdata=levels[i].B[indys, :].real,
+                                fname=fname + "B_variable" + str(j) + "_lvl" + str(i) + ".vtu")
 
-
-    ##
     # Output requested prolongator basis functions
     if Pcols is not None:
-        for (lvl,cols) in Pcols:
+        for (lvl, cols) in Pcols:
             P = levels[lvl].P.tocsc()
-            cell_stuff = {mesh_num_list[lvl] : E2Vlist[lvl]}
-            for i in cols:           
-                Pcol = array(P[:,i].todense())
-                write_vtu(Verts=Vlist[lvl], Cells=cell_stuff, pdata=Pcol, 
-                          fname=fname+"P_lvl"+str(lvl)+"col"+str(i)+".vtu")
-    
-    ##
+            cell_stuff = {mesh_num_list[lvl]: E2Vlist[lvl]}
+            for i in cols:
+                Pcol = array(P[:, i].todense())
+                pyamg.vis.write_vtu(
+                    Verts=Vlist[lvl],
+                    Cells=cell_stuff,
+                    pdata=Pcol,
+                    fname=fname + "P_lvl" + str(lvl) + "col" + str(i) + ".vtu")
+
     # Output the error on the finest level
     if error is not None:
-        error = error.reshape(-1,1)
-        cell_stuff = {mesh_num_list[0] : E2Vlist[0]}
+        error = error.reshape(-1, 1)
+        cell_stuff = {mesh_num_list[0]: E2Vlist[0]}
         if sparse.isspmatrix_bsr(levels[0].A):
             nPDEs = levels[0].A.blocksize[0]
         else:
             nPDEs = 1
         for j in range(nPDEs):
-            indys = arange(j, levels[0].A.shape[0], nPDEs)
-            write_vtu(Verts=Vlist[0], Cells=cell_stuff, pdata=error[indys,:], \
-                      fname=fname+"error_variable"+str(j)+".vtu")
-
-
-
-      
+            indys = np.arange(j, levels[0].A.shape[0], nPDEs)
+            pyamg.vis.write_vtu(Verts=Vlist[0], Cells=cell_stuff, pdata=error[indys, :],
+                                fname=fname + "error_variable" + str(j) + ".vtu")
--- ComplexSymmetric/one_D_helmholtz.py
+++ ComplexSymmetric/one_D_helmholtz.py
@@ -1,11 +1,10 @@
-from numpy import array, zeros, ones, sqrt, ravel, mod, random, inner, conjugate
-from scipy.sparse import csr_matrix, csc_matrix, coo_matrix, bmat, eye
-from scipy import rand, mat, real, imag, linspace, hstack, vstack, exp, cos, sin, pi
-from pyamg.util.linalg import norm
+import numpy as np
+import scipy.sparse as sparse
 import pyamg
-from scipy.optimize import fminbound, fmin
+import scipy.optimize
 
 __all__ = ['one_D_helmholtz', 'min_wave']
+
 
 def min_wave(A, omega, x, tol=1e-5, maxiter=25):
     '''
@@ -31,22 +30,24 @@ def min_wave(A, omega, x, tol=1e-5, maxiter=25):
 
     '''
 
-    x = ravel(x)
+    x = np.ravel(x)
 
     # Define scalar objective function, ignoring the
     # boundaries by only considering A*c at [1:-1]
     def obj_fcn(alpha):
-        c = cos((omega+alpha)*x)
-        Ac = (A*c)[1:-1]
-        return norm(Ac)/norm(c[1:-1])
+        c = np.cos((omega + alpha) * x)
+        Ac = (A * c)[1:-1]
+        return np.linalg.orm(Ac) / np.linalg.norm(c[1:-1])
 
-    (xopt, fval, ierr, numfunc) = fminbound(obj_fcn, -0.99*omega, \
-         0.99*omega, xtol=tol, maxfun=maxiter, full_output=True, disp=0)
+    (xopt, fval, ierr, numfunc) = scipy.optimize.fminbound(
+        obj_fcn, -0.99 * omega,
+        0.99 * omega, xtol=tol, maxfun=maxiter, full_output=True, disp=0)
 
-    #print "Minimizer = %1.4f,  Function Value at Min = %1.4e\nError Flag = %d,\
+    # print "Minimizer = %1.4f,  Function Value at Min = %1.4e\nError Flag = %d,\
     #        Number of function evals = %d" % (xopt, fval, ierr, numfunc)
 
     return xopt
+
 
 def one_D_helmholtz(h, omega=1.0, nplane_waves=2):
     '''
@@ -79,45 +80,46 @@ def one_D_helmholtz(h, omega=1.0, nplane_waves=2):
     '''
 
     # Ensure Repeatability of "random" initial guess
-    random.seed(10)
+    np.random.seed(10)
 
     # Mesh Spacing
-    mesh_h = 1.0/(float(h)-1.0)
+    mesh_h = 1.0 / (float(h) - 1.0)
 
     # Construct Real Operator
-    reA = pyamg.gallery.poisson( (h,), format='csr')
-    reA = reA - mesh_h*mesh_h*omega*omega*\
-          eye(reA.shape[0], reA.shape[1], format='csr')
+    reA = pyamg.gallery.poisson((h,), format='csr')
+    reA = reA - mesh_h * mesh_h * omega * omega *\
+        sparse.eye(reA.shape[0], reA.shape[1], format='csr')
     dimen = reA.shape[0]
 
     # Construct Imaginary Operator
-    imA = csr_matrix( coo_matrix( (array([2.0*mesh_h*omega]), \
-                    (array([0]), array([0]))), shape=reA.shape) )
+    imA = sparse.csr_matrix(sparse.coo_matrix((np.array([2.0 * mesh_h * omega]),
+                            (np.array([0]), np.array([0]))), shape=reA.shape))
 
     # Enforce Radiation Boundary Conditions at first grid point
     reA.data[1] = -2.0
 
     # In order to maintain symmetry scale the first equation by 1/2
-    reA.data[0] = 0.5*reA.data[0]
-    reA.data[1] = 0.5*reA.data[1]
-    imA.data[0] = 0.5*imA.data[0]
+    reA.data[0] = 0.5 * reA.data[0]
+    reA.data[1] = 0.5 * reA.data[1]
+    imA.data[0] = 0.5 * imA.data[0]
 
     # Create complex-valued system
-    complexA = reA + 1.0j*imA
+    complexA = reA + 1.0j * imA
 
     # For this case, the CG (continuous Galerkin) case is the default elements and vertices
     # because there is no DG mesh to speak of
     elements = None
-    vertices = hstack((linspace(-1.0,1.0,h).reshape(-1,1), zeros((h,1))))
+    vertices = np.hstack((np.linspace(-1.0, 1.0, h).reshape(-1, 1),
+                          np.zeros((h, 1))))
 
     # Near null-space modes are 1-D Plane waves: [exp(ikx), i exp(ikx)]
-    B = zeros( (dimen, nplane_waves), dtype=complex )
-    shift = min_wave(complexA, omega, vertices[:,0], tol=1e-9, maxiter=15)
+    B = np.zeros((dimen, nplane_waves), dtype=complex)
+    shift = min_wave(complexA, omega, vertices[:, 0], tol=1e-9, maxiter=15)
     if nplane_waves == 1:
-        B[:,0] = exp(1.0j*(omega+shift)*vertices[:,0])
+        B[:, 0] = np.exp(1.0j * (omega + shift) * vertices[:, 0])
     elif nplane_waves == 2:
-        B[:,0] = cos((omega+shift)*vertices[:,0])
-        B[:,1] = sin((omega+shift)*vertices[:,0])
+        B[:, 0] = np.cos((omega + shift) * vertices[:, 0])
+        B[:, 1] = np.sin((omega + shift) * vertices[:, 0])
 
-    return {'A' : complexA, 'B' : B, 'mesh_h' : mesh_h, \
-            'elements' : elements, 'vertices' : vertices}
+    return {'A': complexA, 'B': B, 'mesh_h': mesh_h,
+            'elements': elements, 'vertices': vertices}
