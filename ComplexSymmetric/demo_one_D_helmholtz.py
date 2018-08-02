12e591f1f413849bc5033f5f959e0723525eefed
Luke Olson <luke.olson@gmail.com>
2018-08-02 07:07:11 -0500

    clean up demo

--- ComplexSymmetric/demo_one_D_helmholtz.py
+++ ComplexSymmetric/demo_one_D_helmholtz.py
@@ -3,95 +3,83 @@ import scipy.linalg as sla
 import matplotlib.pyplot as plt
 import pyamg
 
-from one_D_helmholtz import one_D_helmholtz, min_wave
-from convergence_tools import print_cycle_history
+from one_D_helmholtz import one_D_helmholtz
 
 # Problem parameters
 h = 1024
-mesh_h =  1.0/(float(h)-1.0)
+mesh_h = 1.0 / (float(h) - 1.0)
 points_per_wavelength = 15.0
 
 # Retrieve 1-D Helmholtz Operator
-omega = (2*pi)/(mesh_h*points_per_wavelength)
+omega = (2 * np.pi) / (mesh_h * points_per_wavelength)
 data = one_D_helmholtz(h, omega=omega, nplane_waves=2)
 A = data['A']
 B = data['B']
 vertices = data['vertices']
-numpy.random.seed(625)
-x0 = scipy.rand(A.shape[0])
-b = numpy.zeros_like(x0)
+np.random.seed(625)
+x0 = np.random.rand(A.shape[0])
+b = np.zeros_like(x0)
 
-# Solver Parameters, note the matrix is complex-symmetric, not Hermitian.
-# Hence, symmetry = 'symmetric'.
-smooth=('energy', {'krylov' : 'gmres'})
-SA_solve_args={'cycle':'W', 'maxiter':20, 'tol':1e-8, 'accel' : 'gmres'}
-SA_build_args={'max_levels':10, 'max_coarse':5, 'coarse_solver':'pinv2', \
-               'symmetry':'symmetric'}
-smoother =('gauss_seidel_nr', {'sweep':'symmetric', 'iterations':1})
+# Solver Parameters
+# Note: the matrix is complex-symmetric, not Hermitian. symmetry = 'symmetric'.
+smooth = ('energy', {'krylov': 'gmres'})
+SA_solve_args = {'cycle': 'W', 'maxiter': 20,
+                 'tol': 1e-8, 'accel': 'gmres'}
+SA_build_args = {'max_levels': 10, 'max_coarse': 5, 'coarse_solver': 'pinv2',
+                 'symmetry': 'symmetric'}
+smoother = ('gauss_seidel_nr', {'sweep': 'symmetric', 'iterations': 1})
 
 # Construct solver using the "naive" constant mode for B
-sa = smoothed_aggregation_solver(A, B = ones((A.shape[0],1)),
-            strength=('symmetric', {'theta':0.0}), presmoother=smoother,
-            postsmoother=smoother, smooth=smooth, **SA_build_args)
+sa = pyamg.smoothed_aggregation_solver(A,
+                                       B=np.ones((A.shape[0], 1)),
+                                       strength=('symmetric', {'theta': 0.0}),
+                                       presmoother=smoother,
+                                       postsmoother=smoother,
+                                       smooth=smooth,
+                                       **SA_build_args)
 
 # Solve
 residuals = []
 x = sa.solve(b, x0=x0, residuals=residuals, **SA_solve_args)
-print("\n*************************************************************")
+
 print("*************************************************************")
-print("Using only a constant mode for interpolation yields an inefficient\n" + \
-      "solver, even for typical PyAMG nonsymmetric SA parameters")
-print_cycle_history(residuals, sa, verbose=True, plotting=False)
+print("A single constant mode for interpolation is an inefficient.")
+for i, r in enumerate(residuals):
+    print("residual at iteration {0:2}: {1:^6.2e}".format(i, r))
 
 # Construct solver using the wave-like modes for B
-sa = smoothed_aggregation_solver(A, B = B,
-            strength=('symmetric', {'theta':0.0}), presmoother=smoother,
-            postsmoother=smoother, smooth=smooth, **SA_build_args)
+sa = pyamg.smoothed_aggregation_solver(A,
+                                       B=B,
+                                       strength=('symmetric', {'theta': 0.0}),
+                                       presmoother=smoother,
+                                       postsmoother=smoother,
+                                       smooth=smooth,
+                                       **SA_build_args)
 
 # Solve
 residuals = []
 x = sa.solve(b, x0=x0, residuals=residuals, **SA_solve_args)
-print("\n*************************************************************")
+
 print("*************************************************************")
-print("Note the improved performance from wave-like interpolation.")
-print_cycle_history(residuals, sa, verbose=True, plotting=False)
-
-
-# plot parameters
-fig_width_pt=300.
-inches_per_pt = 1.0/72.27               # Convert pt to inch
-golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
-fig_width = fig_width_pt*inches_per_pt  # width in inches
-fig_height = fig_width*golden_mean      # height in inches
-ratio = 2.95
-fig_size =  [ratio*fig_width,ratio*fig_height]
-params = {'backend': 'ps',
-          'axes.labelsize':  20,
-          'text.fontsize':   20,
-          'axes.titlesize' : 20,
-          'legend.fontsize': 20,
-          'xtick.labelsize': 20,
-          'ytick.labelsize': 20,
-          'text.usetex': True,
-          'figure.figsize': fig_size,
-          'lines.linewidth' : 4}
-pylab.rcParams.update(params)
+print("The performance improves with wave-like interpolation.")
+for i, r in enumerate(residuals):
+    print("residual at iteration {0:2}: {1:^6.2e}".format(i, r))
 
 # plot B vs. the lowest right singular vector, which represents
 # the near null-space, for a segment of the domain
-pylab.figure(1)
-indys = arange(0,min(75,h))
-line_styles = [ "-b", "--m", ":k"]
-leg = []
+f, ax = plt.subplots(1, 1)
+indys = np.arange(0, min(75, h))
+line_styles = ["-b", "--m", ":k"]
 for i in range(B.shape[1]):
-    pylab.plot(vertices[indys,0], real(B[indys,i]), line_styles[i])
-    leg.append('NNS Mode %d'%i)
-[U,S,V] = svd(A.todense())
-V = V.T.copy()
-scale = 0.9/max(real(V[indys,-1]))
-pylab.plot(vertices[indys,0], scale*real(ravel(V[indys,-1])), line_styles[i+1])
-leg.append('Re$(\\nu)$')
+    plt.plot(vertices[indys, 0], np.real(B[indys, i]),
+             line_styles[i], label='NNS Mode {}'.format(i))
 
-pylab.title('Near Null-Space Modes (NNS) vs. Lowest Right Singular Vector $\\nu$')
-pylab.legend(leg)
-pylab.show()
+[U, S, V] = sla.svd(A.todense())
+V = V.T.copy()
+scale = 0.9 / max(np.real(V[indys, -1]))
+plt.plot(vertices[indys, 0], scale * np.real(np.ravel(V[indys, -1])),
+         line_styles[i + 1], label='Re$(\\nu)$')
+
+plt.title('Near Null-Space (NNS) vs. Lowest Right Singular Vector $\\nu$')
+plt.legend(framealpha=1.0)
+plt.show()
