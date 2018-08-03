55e8597ae6a73ded4cfecd4783f63700af7e5d25
Luke Olson <luke.olson@gmail.com>
2018-08-03 14:33:51 -0500

    clean up imports

--- Preconditioning/demo.py
+++ Preconditioning/demo.py
@@ -1,61 +1,69 @@
 # Examples of AMG as a preconditioner
 
-import scipy
-import numpy
-from pyamg.gallery import linear_elasticity, poisson
-from pyamg import smoothed_aggregation_solver, rootnode_solver
+import numpy as np
+import pyamg
+import matplotlib.pyplot as plt
 
 # Create test cases
 trials = []
-A,B = poisson((500,500), format='csr'), None
-trials.append( ('Poisson',A,B) )
-A,B = linear_elasticity((200,200), format='bsr')
-trials.append( ('Elasticity',A,B) )
-   
-print "Show advantages of accleration for two example problems"
-choice = input('\n Input Choice:\n' + \
-            '1:  Run smoothed_aggregation_solver\n' + \
-            '2:  Run rootnode_solver\n' )
+A, B = pyamg.gallery.poisson((500, 500), format='csr'), None
+trials.append(('Poisson', A, B))
+A, B = pyamg.gallery.linear_elasticity((200, 200), format='bsr')
+trials.append(('Elasticity', A, B))
+
+print("Show advantages of accleration for two example problems")
+choice = input('\n Input Choice:\n' +
+               '1:  Run smoothed_aggregation_solver\n' +
+               '2:  Run rootnode_solver\n')
+choice = int(choice)
 if choice == 1:
-    method = smoothed_aggregation_solver
+    method = pyamg.smoothed_aggregation_solver
 elif choice == 2:
-    method = rootnode_solver
+    method = pyamg.rootnode_solver
 else:
     raise ValueError("Enter a choice of 1 or 2")
 
 
-for name,A,B in trials:
+for name, A, B in trials:
     # Construct solver using AMG based on Smoothed Aggregation (SA)
     mls = method(A, B=B)
 
     # Display hierarchy information
-    print 'Matrix: %s' % name
-    print mls
+    print('Matrix: %s' % name)
+    print(mls)
 
     # Create random right hand side
-    b = scipy.rand(A.shape[0],1)
-    
+    b = np.random.rand(A.shape[0], 1)
+
     # Solve Ax=b with no acceleration ('standalone' solver)
     standalone_residuals = []
     x = mls.solve(b, tol=1e-10, accel=None, residuals=standalone_residuals)
-    
+
     # Solve Ax=b with Conjugate Gradient (AMG as a preconditioner to CG)
     accelerated_residuals = []
     x = mls.solve(b, tol=1e-10, accel='cg', residuals=accelerated_residuals)
-    
-    # Compute relative residuals
-    standalone_residuals  = numpy.array(standalone_residuals)/standalone_residuals[0]  
-    accelerated_residuals = numpy.array(accelerated_residuals)/accelerated_residuals[0]  
-    
-    # Plot convergence history
-    import pylab
-    pylab.figure()
-    pylab.title('Convergence History (%s)' % name)
-    pylab.xlabel('Iteration')
-    pylab.ylabel('Relative Residual')
-    pylab.semilogy(standalone_residuals,  label='Standalone',  linestyle='None', marker='.')
-    pylab.semilogy(accelerated_residuals, label='Accelerated', linestyle='None', marker='.')
-    pylab.legend()
-    print "Close window for program to proceed.\n"
-    pylab.show()
 
+    # Compute relative residuals
+    standalone_residuals = np.array(
+        standalone_residuals) / standalone_residuals[0]
+    accelerated_residuals = np.array(
+        accelerated_residuals) / accelerated_residuals[0]
+
+    # Plot convergence history
+    plt.figure()
+    plt.title('Convergence History (%s)' % name)
+    plt.xlabel('Iteration')
+    plt.ylabel('Relative Residual')
+    plt.semilogy(
+        standalone_residuals,
+        label='Standalone',
+        linestyle='None',
+        marker='.')
+    plt.semilogy(
+        accelerated_residuals,
+        label='Accelerated',
+        linestyle='None',
+        marker='.')
+    plt.legend()
+    print("Close window for program to proceed.\n")
+    plt.show()
