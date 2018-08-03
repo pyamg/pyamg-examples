c4ded61b5873ebd30abd23cab36495468fa2cd87
Luke Olson <luke.olson@gmail.com>
2018-08-03 14:05:22 -0500

    clean up fipy interface

--- FiPyFormulation/PyAMGSolver.py
+++ FiPyFormulation/PyAMGSolver.py
@@ -1,26 +1,27 @@
 from fipy.matrices.pysparseMatrix import _PysparseMeshMatrix
 from fipy.solvers.solver import Solver
+import numpy as np
+import scipy.sparse as sparse
+import pyamg
 
-import numpy
-from scipy.sparse import csr_matrix
-from pyamg import smoothed_aggregation_solver
 
 class PyAMGSolver(Solver):
     """
     The PyAMGSolver class.
     """
+
     def __init__(self, *args, **kwargs):
-        if kwargs.has_key('verbosity'):
+        if 'verbosity' in kwargs:
             self.verbosity = kwargs['verbosity']
         else:
             self.verbosity = False
 
-        if kwargs.has_key('MGSetupOpts'):
+        if 'MGSetupOpts' in kwargs:
             self.MGSetupOpts = kwargs['MGSetupOpts']
         else:
             self.MGSetupOpts = {}
 
-        if kwargs.has_key('MGSolveOpts'):
+        if 'MGSolveOpts' in kwargs:
             self.MGSolveOpts = kwargs['MGSolveOpts']
         else:
             self.MGSolveOpts = {}
@@ -30,32 +31,32 @@ class PyAMGSolver(Solver):
         return _PysparseMeshMatrix
 
     def _solve_(self, L, x, b):
-        relres=[]
-        
+        relres = []
+
         # create scipy.sparse matrix view
-        (data,row,col) = L.matrix.find()
-        A = csr_matrix((data,(row,col)),shape=L.matrix.shape)
+        (data, row, col) = L.matrix.find()
+        A = sparse.csr_matrix((data, (row, col)), shape=L.matrix.shape)
 
         # solve and deep copy data
-        ml = smoothed_aggregation_solver(A,**self.MGSetupOpts)
-        x[:] = ml.solve(b=b,residuals=relres,**self.MGSolveOpts)
+        ml = pyamg.smoothed_aggregation_solver(A, **self.MGSetupOpts)
+        x[:] = ml.solve(b=b, residuals=relres, **self.MGSolveOpts)
 
         # fix relres and set info
-        if len(relres)>0:
-            relres=numpy.array(relres)/relres[0]
+        if len(relres) > 0:
+            relres = np.array(relres) / relres[0]
             info = 0
         iter = len(relres)
 
         if self.verbosity:
-            print ml
-            print 'MG iterations: %d'%iter
-            print 'MG convergence factor: %g'%((relres[-1])**(1.0/iter))
-            print 'MG residual history: ', relres
+            print(ml)
+            print('MG iterations: %d' % iter)
+            print('MG convergence factor: %g' % ((relres[-1])**(1.0 / iter)))
+            print('MG residual history: ', relres)
 
         self._raiseWarning(info, iter, relres)
 
     def _solve(self):
         self._solve_(self.matrix, self.var.numericValue, self.RHSvector)
-            
+
     def _canSolveAssymetric(self):
         return False
--- FiPyFormulation/demo.py
+++ FiPyFormulation/demo.py
@@ -1,42 +1,40 @@
 # This example solves a diffusion problem and demonstrates the use of
 # applying boundary condition patches.
 try:
-    from fipy import *
+    import fipy
 except ImportError:
     raise ImportError('Problem with FiPy Installation')
 
 from PyAMGSolver import PyAMGSolver
-#
+
 nx = 20
 ny = nx
 dx = 1.0
 dy = dx
 L = dx * nx
-mesh = Grid2D(dx=dx, dy=dy, nx=nx, ny=ny)
-phi = CellVariable(name = "solution variable", mesh = mesh, value = 0.)
+mesh = fipy.Grid2D(dx=dx, dy=dy, nx=nx, ny=ny)
+phi = fipy.CellVariable(name="solution variable", mesh=mesh, value=0.)
 
 # Apply Dirichlet boundary conditions (else Neumann by default)
 valueTopLeft = 0
 valueBottomRight = 1
 x, y = mesh.getFaceCenters()
-facesTopLeft = ((mesh.getFacesLeft() & (y > L / 2))
-                | (mesh.getFacesTop() & (x < L / 2)))
-facesBottomRight = ((mesh.getFacesRight() & (y < L / 2))
-                    | (mesh.getFacesBottom() & (x > L / 2)))
-BCs = (FixedValue(faces=facesTopLeft, value=valueTopLeft),
-       FixedValue(faces=facesBottomRight, value=valueBottomRight))
+facesTopLeft = ((mesh.getFacesLeft() & (y > L / 2)) | (mesh.getFacesTop() & (x < L / 2)))
+facesBottomRight = ((mesh.getFacesRight() & (y < L / 2)) | (mesh.getFacesBottom() & (x > L / 2)))
+BCs = (fipy.FixedValue(faces=facesTopLeft, value=valueTopLeft),
+       fipy.FixedValue(faces=facesBottomRight, value=valueBottomRight))
 
 # set solver
-#solver = LinearLUSolver(tolerance = 1.e-6, iterations = 100)
-MGSetupOpts = {'max_coarse':10}
-MGSolveOpts = {'maxiter':100, 'tol':1e-8}
-solver = PyAMGSolver(verbosity=1,MGSetupOpts=MGSetupOpts,MGSolveOpts=MGSolveOpts)
+# solver = LinearLUSolver(tolerance = 1.e-6, iterations = 100)
+MGSetupOpts = {'max_coarse': 10}
+MGSolveOpts = {'maxiter': 100, 'tol': 1e-8}
+solver = PyAMGSolver(verbosity=1, MGSetupOpts=MGSetupOpts, MGSolveOpts=MGSolveOpts)
 
 # solve diffusion equation with solver above
-ImplicitDiffusionTerm().solve(var=phi, 
-                              boundaryConditions = BCs,
-                              solver = solver)
+fipy.ImplicitDiffusionTerm().solve(var=phi,
+                                   boundaryConditions=BCs,
+                                   solver=solver)
 
 # view result
-viewer = Viewer(vars=phi, datamin=0., datamax=1.)
+viewer = fipy.Viewer(vars=phi, datamin=0., datamax=1.)
 viewer.plot()
