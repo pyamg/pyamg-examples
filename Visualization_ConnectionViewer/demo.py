a5daeebb2824519eff1eb850276d4d0d27d22236
Luke Olson <luke.olson@gmail.com>
2018-08-03 15:28:02 -0500

    fix name

--- Visualization_ConnectionViewer/demo.py
+++ Visualization_ConnectionViewer/demo.py
@@ -1,12 +1,9 @@
-##
 # Import ConnectionViewer writers
-import numpy, scipy
+# java -jar ~/Downloads/ConnectionViewer.jar test_A0.mat
+import numpy as np
+import pyamg
 
-from pyamg import gallery, rootnode_solver
-from pyamg.gallery import stencil_grid
-from pyamg.gallery.diffusion import diffusion_stencil_2d
-
-from cvoutput import *
+from cvoutput import outputML
 from convergence_tools import print_cycle_history
 
 ##
@@ -14,32 +11,34 @@ from convergence_tools import print_cycle_history
 n = 10
 nx = n
 ny = n
-stencil = diffusion_stencil_2d(type='FE',epsilon=0.001,theta=scipy.pi/3)
-A = stencil_grid(stencil, (nx,ny), format='csr')
-numpy.random.seed(625)
-x = scipy.rand(A.shape[0])
-b = A*scipy.rand(A.shape[0])
+stencil = pyamg.gallery.diffusion_stencil_2d(type='FE', epsilon=0.001, theta=np.pi / 3)
+A = pyamg.gallery.stencil_grid(stencil, (nx, ny), format='csr')
+np.random.seed(625)
+x = np.random.rand(A.shape[0])
+b = A * np.random.rand(A.shape[0])
 
-ml = rootnode_solver(A, strength=('evolution', {'epsilon':2.0}), 
-                    smooth=('energy', {'degree':2}), max_coarse=10)
+ml = pyamg.rootnode_solver(A, strength=('evolution', {'epsilon': 2.0}),
+                           smooth=('energy', {'degree': 2}), max_coarse=10)
 resvec = []
 x = ml.solve(b, x0=x, maxiter=20, tol=1e-14, residuals=resvec)
 print_cycle_history(resvec, ml, verbose=True, plotting=False)
 
 ##
 # Write ConnectionViewer files for multilevel hierarchy ml
-xV,yV = numpy.meshgrid(numpy.arange(0,ny,dtype=float),numpy.arange(0,nx,dtype=float))
-Verts = numpy.concatenate([[xV.ravel()],[yV.ravel()]],axis=0).T
+xV, yV = np.meshgrid(
+    np.arange(
+        0, ny, dtype=float), np.arange(
+            0, nx, dtype=float))
+Verts = np.concatenate([[xV.ravel()], [yV.ravel()]], axis=0).T
 outputML("test", Verts, ml)
 
 
-print "\n\nOutput files for matrix stencil visualizations in ConnectionViewer are: \n  \
+print("\n\nOutput files for matrix stencil visualizations in ConnectionViewer are: \n  \
 test_A*.mat \n  test_fine*.marks \n  test_coarse*.marks \n  \
-test_R*.mat \n  test_P*.mat \nwhere \'*\' is the level number"
+test_R*.mat \n  test_P*.mat \nwhere \'*\' is the level number")
 ##
-print "\n\nYou can download ConnectionViewer from \nhttp://gcsc.uni-frankfurt.de/Members/mrupp/connectionviewer/ \n\nWhen you open test_A0.mat with ConnectionViewer, you'll get"
+print("\n\nYou can download ConnectionViewer from \nhttp://gcsc.uni-frankfurt.de/Members/mrupp/connectionviewer/ \n\nWhen you open test_A0.mat with ConnectionViewer, you'll get")
 ##
-print "\nIn ConnectionViewer, you can zoom in with the mousewheel\n \
+print("\nIn ConnectionViewer, you can zoom in with the mousewheel\n \
 and drag the grid around. By clicking on a node, you can see its\n \
-matrix connections."
-
+matrix connections.")
--- util/convergence_tools.py
+++ util/convergence_tools.py
@@ -5,6 +5,7 @@ import numpy
 import scipy
 from pyamg.util.utils import print_table
 
+
 def print_cycle_history(resvec, ml, verbose=False, plotting=False):
     """
     Shows a summary of the complexity, convergence factors, and total work
@@ -27,50 +28,65 @@ def print_cycle_history(resvec, ml, verbose=False, plotting=False):
 
         - A-mean refers to the current or running arithmetic mean
 
-        - G-mean refers to the geometric mean (current or 
+        - G-mean refers to the geometric mean (current or
 
         - Work is the estimated total work needed to reduce the residual by a
             factor of 10, based on the geometric mean of the convergence factors
             and on the cycle complexity
     """
     resvec = numpy.array(resvec)
-    factors = resvec[1:]/resvec[0:-1]
+    factors = resvec[1:] / resvec[0:-1]
 
     print('---Convergence Summary---------------------------------------')
     print('')
-    avg_convergence_factor = (resvec[-1]/resvec[0])**(1.0/len(resvec))
+    avg_convergence_factor = (resvec[-1] / resvec[0])**(1.0 / len(resvec))
     print('             Levels: %d' % len(ml.levels))
     print('   Cycle Complexity: %6.3f' % ml.cycle_complexity())
     print('Operator Complexity: %6.3f' % ml.operator_complexity())
     print('    Grid Complexity: %6.3f' % ml.grid_complexity())
     print('avg geo conv factor: %6.3f' % avg_convergence_factor)
-    print('               work: %6.3f' % (-ml.cycle_complexity() / scipy.log10(avg_convergence_factor)))
+    print('               work: %6.3f' %
+          (-ml.cycle_complexity() / scipy.log10(avg_convergence_factor)))
     print('')
 
-    total_nnz =  sum([level.A.nnz for level in ml.levels])
+    total_nnz = sum([level.A.nnz for level in ml.levels])
     print('level   unknowns     nnz')
-    for n,level in enumerate(ml.levels):
+    for n, level in enumerate(ml.levels):
         A = level.A
-        print('   %-2d   %-10d   %-10d [%5.2f%%]' % (n,A.shape[1],A.nnz,(100*float(A.nnz)/float(total_nnz))))
+        print(
+            '   %-2d   %-10d   %-10d [%5.2f%%]' %
+            (n,
+             A.shape[1],
+             A.nnz,
+             (100 *
+              float(
+                  A.nnz) /
+                 float(total_nnz))))
 
     if(verbose):
         print('')
         print('---Convergence Summary (verbose)-----------------------------')
-        print('%20s'%'Factors:')
-        plist = ('iter', 'Factor', 'A-Mean','G-Mean', 'Work')
+        print('%20s' % 'Factors:')
+        plist = ('iter', 'Factor', 'A-Mean', 'G-Mean', 'Work')
         print('%-10s %-10s %-10s %-10s %-10s' % plist)
 
-        for i in range(0,len(resvec)):
-            if(i>0):
-                iresvec = resvec[:(i+1)]                            # running list of residuals
-                ifactors = iresvec[1:]/iresvec[0:-1]                # running list of factors
+        for i in range(0, len(resvec)):
+            if(i > 0):
+                # running list of residuals
+                iresvec = resvec[:(i + 1)]
+                # running list of factors
+                ifactors = iresvec[1:] / iresvec[0:-1]
 
-                ifactor = ifactors[-1]                              # current arith mean
-                aafactor = numpy.mean(ifactors)                     # running arith mean
-                gafactor = (iresvec[-1]/iresvec[0])**(1.0/(i+1))    # geometric mean
+                # current arith mean
+                ifactor = ifactors[-1]
+                # running arith mean
+                aafactor = numpy.mean(ifactors)
+                gafactor = (iresvec[-1] / iresvec[0]
+                            )**(1.0 / (i + 1))    # geometric mean
 
                 ocx = ml.cycle_complexity()
-                iwork = - ocx / scipy.log10(gafactor)               # current work-per-digit
+                # current work-per-digit
+                iwork = - ocx / scipy.log10(gafactor)
 
                 plist = (i, ifactor, aafactor, gafactor, iwork)
                 print('%-10d %-10.3f %-10.3f %-10.3f %-10.3f' % plist)
@@ -80,12 +96,19 @@ def print_cycle_history(resvec, ml, verbose=False, plotting=False):
     if(plotting):
         from pylab import plot, show, semilogy, figure, xlabel, ylabel, axis
         figure(1)
-        semilogy(resvec,'k')
+        semilogy(resvec, 'k')
         xlabel('iteration')
         ylabel('residual')
         show()
 
-def print_scalability(factors,complexity,nnz,nlist,plotting=False,title='Scalability Test'):
+
+def print_scalability(
+        factors,
+        complexity,
+        nnz,
+        nlist,
+        plotting=False,
+        title='Scalability Test'):
     """
     Shows a summary of the scalability in problems size n.
 
@@ -109,23 +132,25 @@ def print_scalability(factors,complexity,nnz,nlist,plotting=False,title='Scalabi
     # build table up in list form, then pass to print_table for pretty output
     table = []
 
-    table.append(["n","nnz","rho","OpCx","Work"])
-    run=0
-    work = numpy.zeros((len(nlist),1)).ravel()
+    table.append(["n", "nnz", "rho", "OpCx", "Work"])
+    run = 0
+    work = numpy.zeros((len(nlist), 1)).ravel()
     for n in nlist:
-        work[run]=-complexity[run]/scipy.log10(factors[run]);
-        table.append([ str(int(n)), str(int(nnz[run])), ('%1.3f'%factors[run]), ('%1.3f'%complexity[run]), ('%1.3f'%work[run]) ])
-        run+=1
+        work[run] = -complexity[run] / scipy.log10(factors[run])
+        table.append([str(int(n)), str(int(nnz[run])), ('%1.3f' %
+                                                        factors[run]), ('%1.3f' %
+                                                                        complexity[run]), ('%1.3f' %
+                                                                                           work[run])])
+        run += 1
 
     print(print_table(table, title=title, col_padding=4))
 
     if(plotting):
         from pylab import plot, show, semilogy, figure, xlabel, ylabel, axis
         figure(1)
-        maxscale = max([work.max(),2])
-        plot(nlist[1:],work[1:],'k')
-        axis([nlist[1],nlist[-1],0,1.1*maxscale])
+        maxscale = max([work.max(), 2])
+        plot(nlist[1:], work[1:], 'k')
+        axis([nlist[1], nlist[-1], 0, 1.1 * maxscale])
         xlabel('n')
         ylabel('work per digit of accuracy')
         show()
-
