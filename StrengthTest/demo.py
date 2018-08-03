d7f5d91357ba62a0c658d8b85af6787060c1044a
Luke Olson <luke.olson@gmail.com>
2018-08-03 15:18:08 -0500

    fix int

--- StrengthTest/demo.py
+++ StrengthTest/demo.py
@@ -1,39 +1,44 @@
 import numpy as np
-import scipy as sp
-from scipy.linalg import norm
-from pyamg import *
-from pyamg.gallery import stencil_grid
-from pyamg.gallery.diffusion import diffusion_stencil_2d
+import pyamg
+import matplotlib.pyplot as plt
 
-n=1e2
-stencil = diffusion_stencil_2d(type='FE',epsilon=0.001,theta=sp.pi/3)
-A = stencil_grid(stencil, (n,n), format='csr')
-b = sp.rand(A.shape[0])
+n = int(1e2)
+stencil = pyamg.gallery.diffusion_stencil_2d(type='FE', epsilon=0.001, theta=np.pi / 3)
+A = pyamg.gallery.stencil_grid(stencil, (n, n), format='csr')
+b = np.random.rand(A.shape[0])
 x0 = 0 * b
 
 runs = []
 options = []
-options.append(('symmetric', {'theta' : 0.0 }))
-options.append(('symmetric', {'theta' : 0.25}))
-options.append(('evolution', {'epsilon' : 4.0}))
-options.append(('algebraic_distance', {'epsilon' : 2.0, 'p' : np.inf, 'R' : 10, 'alpha' : 0.5, 'k' : 20}))
-options.append(('algebraic_distance', {'epsilon' : 3.0, 'p' : np.inf, 'R' : 10, 'alpha' : 0.5, 'k' : 20}))
-options.append(('affinity', {'epsilon' : 3.0, 'R' : 10, 'alpha' : 0.5, 'k' : 20}))
-options.append(('affinity', {'epsilon' : 4.0, 'R' : 10, 'alpha' : 0.5, 'k' : 20}))
+options.append(('symmetric', {'theta': 0.0}))
+options.append(('symmetric', {'theta': 0.25}))
+options.append(('evolution', {'epsilon': 4.0}))
+options.append(
+    ('algebraic_distance', {
+        'epsilon': 2.0, 'p': np.inf, 'R': 10, 'alpha': 0.5, 'k': 20}))
+options.append(
+    ('algebraic_distance', {
+        'epsilon': 3.0, 'p': np.inf, 'R': 10, 'alpha': 0.5, 'k': 20}))
+options.append(('affinity', {'epsilon': 3.0, 'R': 10, 'alpha': 0.5, 'k': 20}))
+options.append(('affinity', {'epsilon': 4.0, 'R': 10, 'alpha': 0.5, 'k': 20}))
 
 for opt in options:
-    optstr = opt[0]+'\n    '+',\n    '.join(['%s=%s'%(u,v) for (u,v) in opt[1].items()])
-    print "running %s"%(optstr)
+    optstr = opt[0] + '\n    ' + \
+        ',\n    '.join(['%s=%s' % (u, v) for (u, v) in list(opt[1].items())])
+    print("running %s" % (optstr))
 
-    ml = smoothed_aggregation_solver(A, strength = opt, max_levels=10, max_coarse=5, keep=False)
+    ml = pyamg.smoothed_aggregation_solver(
+        A,
+        strength=opt,
+        max_levels=10,
+        max_coarse=5,
+        keep=False)
     res = []
-    x = ml.solve(b, x0, tol=1e-12,residuals=res)
-    runs.append((res,optstr))
+    x = ml.solve(b, x0, tol=1e-12, residuals=res)
+    runs.append((res, optstr))
 
-import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = plt.subplot(111)
-ax.hold(True)
 for run in runs:
     ax.semilogy(run[0], label=run[1], linewidth=3)
 ax.set_xlabel('Iteration')
