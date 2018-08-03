f8e52009a6edea07f1cfc32aa57d88186e312a10
Luke Olson <luke.olson@gmail.com>
2018-08-03 14:26:42 -0500

    fix plotting params

--- OneDimension/oneD_tools.py
+++ OneDimension/oneD_tools.py
@@ -5,21 +5,16 @@ and columns of P.  This is possible on any level
 """
 __docformat__ = "restructuredtext en"
 
-import warnings
-
 __all__ = ['oneD_P_vis', 'oneD_coarse_grid_vis', 'oneD_profile']
 
-import pylab
-from numpy import ravel, zeros, ones, min, abs, array, max, pi
-from scipy import imag, real, linspace, exp, rand, sqrt, mod
-from scipy.sparse import bsr_matrix
-from scipy.linalg import solve 
-from pyamg.util.linalg import norm
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def update_rcparams(fig_width_pt=700.0):
     '''
     Updates rcparams for appropriate plotting parameters
-    
+
     parameters
     ----------
     fig_width_pt : {float}
@@ -43,29 +38,28 @@ def update_rcparams(fig_width_pt=700.0):
 
     '''
 
-    inches_per_pt = 1.0/72.27               # Convert pt to inch
-    golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
-    fig_width = fig_width_pt*inches_per_pt  # width in inches
-    fig_height = fig_width*golden_mean      # height in inches
-    fig_size =  [fig_width,fig_height]
+    inches_per_pt = 1.0 / 72.27               # Convert pt to inch
+    golden_mean = (np.sqrt(5) - 1.0) / 2.0         # Aesthetic ratio
+    fig_width = fig_width_pt * inches_per_pt  # width in inches
+    fig_height = fig_width * golden_mean      # height in inches
+    fig_size = [fig_width, fig_height]
 
     params = {'backend': 'ps',
               'axes.labelsize': 24,
-              'text.fontsize': 24,
-              'axes.titlesize' : 24,
+              'axes.titlesize': 24,
               'legend.fontsize': 22,
               'xtick.labelsize': 22,
               'ytick.labelsize': 22,
               'text.usetex': True,
-              'linewidth' : 2,
+              'lines.linewidth': 2,
               'figure.figsize': fig_size}
-    
-    pylab.rcParams.update(params)
+
+    plt.rcParams.update(params)
 
 
 def oneD_profile(mg, grid=None, x0=None, b=None, iter=1, cycle='V', fig_num=1):
     '''
-    Profile mg on the problem defined by x0 and b.  
+    Profile mg on the problem defined by x0 and b.
     Default problem is x0=rand, b = 0.
 
     Parameters
@@ -86,7 +80,7 @@ def oneD_profile(mg, grid=None, x0=None, b=None, iter=1, cycle='V', fig_num=1):
         solve with a V, W or F cycle
     fig_num : int
         figure number from which to begin plotting
-    
+
     Returns
     -------
     The residual ratio history are sent to the plotter.
@@ -116,51 +110,58 @@ def oneD_profile(mg, grid=None, x0=None, b=None, iter=1, cycle='V', fig_num=1):
     ndof = mg.levels[0].A.shape[0]
 
     # Default regular grid on 0 to 1
-    if grid == None:
-        grid = linspace(0,1,ndof)
-    elif ravel(grid).shape[0] != ndof:
+    if grid is None:
+        grid = np.linspace(0, 1, ndof)
+    elif np.ravel(grid).shape[0] != ndof:
         raise ValueError("Grid must be of size %d" % ndof)
 
     # Default initial guess is random
-    if x0 == None:
-        x0 = rand(ndof,)
+    if x0 is None:
+        x0 = np.random.rand(ndof,)
         if A.dtype == complex:
-            x0 += 1.0j*rand(ndof,)
-    elif ravel(x0).shape[0] != ndof:
+            x0 += 1.0j * np.random.rand(ndof,)
+    elif np.ravel(x0).shape[0] != ndof:
         raise ValueError("Initial guess must be of size %d" % ndof)
     else:
-        x0 = ravel(x0)
+        x0 = np.ravel(x0)
 
     # Default RHS is all zero
-    if b == None:
-        b = zeros((ndof,), dtype=A.dtype)
-    elif ravel(b).shape[0] != ndof:
+    if b is None:
+        b = np.zeros((ndof,), dtype=A.dtype)
+    elif np.ravel(b).shape[0] != ndof:
         raise ValueError("RHS must be of size %d" % ndof)
     else:
-        b = ravel(b)
-   
+        b = np.ravel(b)
+
     # solve system with mg
     res = []
-    guess = mg.solve(b, x0=x0, tol=1e-8, maxiter=iter, cycle=cycle, residuals=res)
-    res = array(res)
-    resratio = res[1:]/res[0:-1]
-    r = b - A*guess
+    guess = mg.solve(
+        b,
+        x0=x0,
+        tol=1e-8,
+        maxiter=iter,
+        cycle=cycle,
+        residuals=res)
+    res = np.array(res)
+    resratio = res[1:] / res[0:-1]
+    r = b - A * guess
+    print(np.linalg.norm(r))
 
     # plot results
     if iter > 1:
-        pylab.figure(fig_num)
-        pylab.plot(array(range(1,resratio.shape[0]+1)), resratio)
-        pylab.title('Residual Ratio History')
-        pylab.xlabel('Iteration')
-        pylab.ylabel(r'$||r_{i}|| / ||r_{i-1}||$')
-        pylab.xticks(array(range(1,resratio.shape[0]+1)))
+        plt.figure(fig_num)
+        plt.plot(np.array(range(1, resratio.shape[0] + 1)), resratio)
+        plt.title('Residual Ratio History')
+        plt.xlabel('Iteration')
+        plt.ylabel(r'$||r_{i}|| / ||r_{i-1}||$')
+        plt.xticks(np.array(range(1, resratio.shape[0] + 1)))
 
 
 def oneD_coarse_grid_vis(mg, fig_num=1, level=0):
     '''
-    Visualize the aggregates on level=level in terms of 
+    Visualize the aggregates on level=level in terms of
     the aggregates' fine grid representation
-    
+
     Parameters
     ----------
     mg : pyamg multilevel hierarchy
@@ -192,47 +193,50 @@ def oneD_coarse_grid_vis(mg, fig_num=1, level=0):
     >>>pylab.show()
 
     '''
-    
+
     update_rcparams()
     colors = ['b', 'r', 'g', 'k', 'c', 'm', 'y']
 
-    if level > (len(mg.levels)-2):
+    if level > (len(mg.levels) - 2):
         raise ValueError("Level %d has no AggOp" % level)
 
     # Retrieve and map Aggregate to finest level
     ndof = mg.levels[0].A.shape[0]
 
-    if not hasattr(mg.levels[level],'AggOp'):
-        raise ValueError('AggOp needed in hierarchy.  Run the setup with the \'keep\' flag')
+    if not hasattr(mg.levels[level], 'AggOp'):
+        raise ValueError(
+            'AggOp needed in hierarchy.  Run the setup with the \'keep\' flag')
 
     AggOp = mg.levels[level].AggOp
-    for i in range(level-1,-1,-1):
-        AggOp = mg.levels[i].AggOp*AggOp
+    for i in range(level - 1, -1, -1):
+        AggOp = mg.levels[i].AggOp * AggOp
 
     AggOp = AggOp.tocsc()
 
     # Default grid
-    x = array(range(ndof))
+    x = np.array(range(ndof))
 
     # Plot each aggregate
-    pylab.figure(fig_num)
+    plt.figure(fig_num)
     for i in range(AggOp.shape[1]):
-        aggi = AggOp[:,i].indices
-        pylab.plot(x[aggi], i*ones((aggi.shape[0],)), colors[mod(i, len(colors))], marker='o', linewidth=2, markersize=12)
-    
-    title_string='Level ' + str(level) + ' Aggregates'
+        aggi = AggOp[:, i].indices
+        plt.plot(x[aggi], i * np.ones((aggi.shape[0],)),
+                 colors[np.mod(i, len(colors))],
+                 marker='o', linewidth=2, markersize=12)
+
+    title_string = 'Level ' + str(level) + ' Aggregates'
     if level != 0:
         title_string += '\nMapped to Finest Level'
-    pylab.title(title_string)
-    pylab.xlabel('DOF')
-    pylab.ylabel('Aggregate Number')
-    pylab.axis([min(x)-.05, max(x)+.05, -1, AggOp.shape[1]])
-    
+    plt.title(title_string)
+    plt.xlabel('DOF')
+    plt.ylabel('Aggregate Number')
+    plt.axis([min(x) - .05, max(x) + .05, -1, AggOp.shape[1]])
+
 
 def oneD_P_vis(mg, fig_num=1, level=0, interp=False):
     '''
     Visualize the basis functions of P (i.e. columns) from level=level
-    
+
     Parameters
     ----------
     mg : pyamg multilevel hierarchy
@@ -242,8 +246,8 @@ def oneD_P_vis(mg, fig_num=1, level=0, interp=False):
     level : int
         level on which to visualize
     interp : {True, False}
-        Should the columns of P be interpolated to the finest 
-        level before plotting? i.e., plot the columns of 
+        Should the columns of P be interpolated to the finest
+        level before plotting? i.e., plot the columns of
         (P_1*P_2*...*P_level) or just columns of P_level
 
     Returns
@@ -253,7 +257,7 @@ def oneD_P_vis(mg, fig_num=1, level=0, interp=False):
 
     Notes
     -----
-    These plots are only useful for small grids as all columns 
+    These plots are only useful for small grids as all columns
     of P are actually printed
 
     Examples
@@ -270,53 +274,64 @@ def oneD_P_vis(mg, fig_num=1, level=0, interp=False):
     >>>oneD_P_vis(ml, level=1, interp=True)
     >>>pylab.show()
     '''
-    
+
     update_rcparams()
     colors = ['b', 'r', 'g', 'k', 'c', 'm', 'y']
 
-    if level > (len(mg.levels)-2):
+    if level > (len(mg.levels) - 2):
         raise ValueError("Level %d has no P" % level)
 
     # Retrieve P from appropriate level
-    if interp == False:
+    if not interp:
         ndof = mg.levels[level].A.shape[0]
         P = mg.levels[level].P
-    else:     
+    else:
         # Interpolate P to the finest level
         ndof = mg.levels[0].A.shape[0]
         P = mg.levels[level].P
-        for i in range(level-1,-1,-1):
-            P = mg.levels[i].P*P
+        for i in range(level - 1, -1, -1):
+            P = mg.levels[i].P * P
 
     blocks = P.blocksize[1]
     P = P.tocsc()
 
     # Default grid
-    x = array(range(ndof))
-    
+    x = np.array(range(ndof))
+
     # Grab and plot each aggregate's part of a basis function together
     for i in range(0, P.shape[1], blocks):
-        #extract aggregate i's basis functions
-        p = P[:, i:(i+blocks)].todense()
+        # extract aggregate i's basis functions
+        p = P[:, i:(i + blocks)].todense()
         for j in range(blocks):
-            pylab.figure(fig_num+j)
-            p2 = ravel(p[:,j])
-            pylab.plot(x[p2!=0], real(p2[p2!=0.0]), colors[mod(i, len(colors))], marker='o', linewidth=2, markersize=12)
-            title_string = ('Level ' + str(level) + '\nReal Part of %d-th Local Interp Fcn' % j)
+            plt.figure(fig_num + j)
+            p2 = np.ravel(p[:, j])
+            plt.plot(x[p2 != 0], np.real(p2[p2 != 0.0]),
+                     colors[np.mod(i, len(colors))],
+                     marker='o', linewidth=2, markersize=12)
+            title_string = (
+                'Level ' +
+                str(level) +
+                '\nReal Part of %d-th Local Interp Fcn' %
+                j)
             if interp and (level != 0):
                 title_string += '\nInterpolated to Finest Level'
-            pylab.title(title_string)
-            pylab.xlabel('DOF')
-            pylab.ylabel('real(Local Interp Fcn)')
-            
+            plt.title(title_string)
+            plt.xlabel('DOF')
+            plt.ylabel('real(Local Interp Fcn)')
+
             if P.dtype == complex:
-                pylab.figure(fig_num+10+j)
-                p2 = ravel(p[:,j])
-                pylab.plot(x[p2!=0], imag(p2[p2!=0.0]), colors[mod(i, len(colors))], marker='o', linewidth=2, markersize=12)
-                title_string = ('Level ' + str(level) + '\nImag Part of %d-th Local Interp Fcn' % j)
+                plt.figure(fig_num + 10 + j)
+                p2 = np.ravel(p[:, j])
+                plt.plot(x[p2 != 0], np.imag(p2[p2 != 0.0]),
+                         colors[np.mod(i, len(colors))],
+                         marker='o', linewidth=2, markersize=12)
+                title_string = (
+                    'Level ' +
+                    str(level) +
+                    '\nImag Part of %d-th Local Interp Fcn' %
+                    j)
                 if interp and (level != 0):
                     title_string += '\nInterpolated to Finest Level'
-                pylab.title(title_string)
-                pylab.xlabel('Grid')
-                pylab.ylabel('imag(Local Interp Fcn)')
-
+                plt.title(title_string)
+                plt.xlabel('Grid')
+                plt.ylabel('imag(Local Interp Fcn)')
